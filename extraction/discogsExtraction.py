f6200c9e106e678c15dba18b1efedbfb544a16e4
scottpmchugh <scottpmchugh@gmail.com>
2023-10-29 14:20:03 -0500

    updates

--- extraction/discogsExtraction.py
+++ extraction/discogsExtraction.py
@@ -1,11 +1,16 @@
 import discogs_client
 import config
+import pandas as pd
 
 #Connect using auth token
 d = discogs_client.Client('discogsAnalytics', user_token=config.discogs_token)
 me=d.identity()
 
+collection_data = []
 for album in me.collection_folders[1].releases:
-   print([album.release.title],[album.release.artists_sort],[album.release.genres])
-
-  
+   data = [album.release.artists[0].name,
+   album.release.title,
+   album.release.genres[0]]
+   collection_data.append(data)
+df = pd.DataFrame(collection_data, columns=['Artists','Title','Genre'])
+print(df)
